b1dfbd538722c140e4c9c52fbf7be8b3cd48ffda
Creampanda <33093741+Creampanda@users.noreply.github.com>
2020-11-10 14:27:42 +0300

    Update ieee754.s

--- ieee754.s
+++ ieee754.s
@@ -1,72 +1,68 @@
 .global __start
 
-.data
-  num1: .long 0xc5a5fdc0 # сохраняем первое число -5311.71875
-  num2: .long 0x424e2000 # второе число 51.53125  -5260.1875 0xc5a46180
-
 .text
 
 __start:
-  li  s1, 0x45a4f500 # сохраняем первое число 5278.625
-  li  s2, 0xc583b1f0 # второе число -4214.2421875  1064.3828125 0x44850c40
-  xor  s7, s1, s2 # находим знаковый бит 
-  li   s4, 0x7f800000 # константа для нахождения экспоненты
+  li  s1, 0x45a4f500 # first num 5278.625
+  li  s2, 0xc583b1f0 # second num -4214.2421875  #answer =  1064.3828125 = 0x44850c40
+  xor  s7, s1, s2 # getting sign bit 
+  li   s4, 0x7f800000 # constant for getting exp
 before_cmp:  
-  and  t1, s1, s4 # в t1 - первая экспонента
-  mv   s5, t1 # сохраним экспоненту
-  and  t2, s2, s4 # t2 - вторая экспонента
-  bgt  t1, t2, _main # если t1>t2, то идем на марку
-  bne  t1, t2, change_operands #если t1 != t2 то прыгаем менять операнды
-  li   s4,0x007fffff # константа для нахождения мантиссы
-  and  t1, s1, s4 #сравниваем мантиссы, если экспоненты равны
+  and  t1, s1, s4 # в t1 - first exp
+  mv   s5, t1 # saving exp
+  and  t2, s2, s4 # t2 - second exp
+  bgt  t1, t2, _main # if t1>t2 jump main
+  bne  t1, t2, change_operands #if t1 != t2 go swap operands
+  li   s4,0x007fffff # const for mantissa
+  and  t1, s1, s4 # if exponents equal, compare mantiss
   and  t2, s2, s4
-  bgt  t1, t2, _main # если t1<t2 меняем местами
+  bgt  t1, t2, _main # if t1<t2 swap
 change_operands:
   mv   s3, s1
   mv   s1, s2
   mv   s2, s3
   j before_cmp
 _main:
-  srli t1, t1, 0x17 # сдвигаем экспоненту к младшим битами
+  srli t1, t1, 0x17 # move exp to lower bits
   srli t2, t2, 0x17
-  sub  t3, t1, t2 # находим разницу в экспонентах
-  li   s4,0x007fffff # константа для нахождения мантиссы
-  and  t1, s1, s4 #выделяем мантиссу из операндов
+  sub  t3, t1, t2 # get difference in exps
+  li   s4,0x007fffff # const for mantissa
+  and  t1, s1, s4 # get mantissa from operands
   and  t2, s2, s4
-  li   s4, 0x00800000 # константа для прибавления 1. 
-  or   t1, t1, s4 #приводим к нормализованному виду
+  li   s4, 0x00800000 # const for get normalized num 
+  or   t1, t1, s4 # making nums normalized
   or   t2, t2, s4
-  srl  t2, t2, t3 #сдвигаем на разницу
-  bltz s7, different_signs # если разных знаков  #Branch if < zero
-  add  t4, t1, t2 #находим мантиссу
+  srl  t2, t2, t3 # shift the difference
+  bltz s7, different_signs # if numbers different signs 
+  add  t4, t1, t2 # get mantissa
   #if >= 0x01000000
   li   t5, 0x00ffffff 
-  ble  t4, t5, finish #Смотрим нужно ли нам увеличить экспоненту 
+  ble  t4, t5, finish # watch if need to change exp 
   li   t5, 0x00800000
-  add  s5, s5, t5 # Увеличиваем
+  add  s5, s5, t5 # adding to exp
   srli t4, t4, 0x1  
 finish:
-  li   s4, 0x007fffff # константа для нахождения мантиссы
-  and  t4, t4, s4 # получаем мантиссу
-  add  s5, s5, t4 # складываем мантиссу и экспоненту
-  #если у нас большее число положительное - то результат положительный
-  #если большее число отрицательное - то результат отрицательный
+  li   s4, 0x007fffff # const for mantissa
+  and  t4, t4, s4 # get mantissa
+  add  s5, s5, t4 # adding mantissa to exp
+  # if the bigger number is positive - answer is positive
+  # if the bigger number is negative - answer is negative
   bgtz s1, first_positive #Branch if > zero
   li   s4, 0x80000000 # -0
-  or   s5, s5, s4 #делаем результат отрицательным
+  or   s5, s5, s4 # make answer negative
 first_positive:
   addi s5, s5, 0
   j first_positive
   
 
 different_signs:
-  sub  t4, t1, t2 #находим мантиссу
-  beqz t4, decrease_exp #если после вычитания получился ноль, то уменьшаем экспоненту
+  sub  t4, t1, t2 # get mantissa
+  beqz t4, decrease_exp # if we get 0 we decrease exp
 loop:
   bge  t4, s4, finish
-  slli t4, t4, 0x1 # приводим к нормализированному виду путем сдвига влево
-  sub  s5, s5, s4 # уменьшаем экспоненту
+  slli t4, t4, 0x1 # shift left to make num normalized
+  sub  s5, s5, s4 # decreae exp
   j loop
 decrease_exp:
-  sub  s5, s5, s4 # уменьшаем экспоненту
+  sub  s5, s5, s4 # decrease exp
   j finish
